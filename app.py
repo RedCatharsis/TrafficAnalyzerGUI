e90e284ef239a5f751bd00555d9c6d46a6b5a8d0
Janmejay Punetha <janmejaypunetha2@gmail.com>
2024-05-05 15:51:15 +0530

    First

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/download_button')
+def download_button():
+    return render_template('download_button.html')
+
+@app.route('/download_output')
+def download_output():
+    return render_template('download_output.html')
+
+@app.route('/dropdown_list')
+def dropdown_list():
+    return render_template('dropdown_list.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
