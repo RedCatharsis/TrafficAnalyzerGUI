3deb3a70a7e0174844e0a088a5419711ef2856d2
niyazbadar <67091185+niyazbadar@users.noreply.github.com>
2024-05-06 21:10:50 +0530

    popup jank code fix

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, get_flashed_messages, url_for
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    message = get_flashed_messages(with_categories=True)
+    return render_template('index.html', message=message)
 
 #test code for button
-@app.route('/scan', methods=['POST'])  # New route for scan action
+@app.route('/scan', methods=['POST'])
 def scan():
-    
     if request.method == 'POST':
         return "Scanning network..."
     else:
         return "Invalid request method"
+
+@app.route('/downloadButton', methods=['POST'])
+def show_popup():
+    flash('This is a popup message!', 'downloading file...') 
+    return redirect(url_for('index'))  # Redirect back to the index page
 
 if __name__ == "__main__":
     app.run(debug=True)
