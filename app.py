2eab0611d16c6e0bd0e2a3991380963d337b8648
niyazbadar <67091185+niyazbadar@users.noreply.github.com>
2024-05-06 20:39:00 +0530

    python button text
    trying to trigger python code by button click

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return render_template('base.html')
     return render_template('index.html') 
 
-@app.route('/download_button')
-def download_button():
-    return render_template('download_button.html')
-
-@app.route('/download_output')
-def download_output():
-    return render_template('download_output.html')
-
-@app.route('/dropdown_list')
-def dropdown_list():
-    return render_template('dropdown_list.html')
+#test code for button
+@app.route('/scan', methods=['POST'])  # New route for scan action
+def scan():
+    
+    if request.method == 'POST':
+        return "Scanning network..."
+    else:
+        return "Invalid request method"
 
 if __name__ == "__main__":
     app.run(debug=True)
