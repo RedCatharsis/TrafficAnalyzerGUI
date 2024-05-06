f90eecaf65795d55a250d903fdbaf0cf6e95ec33
niyazbadar <67091185+niyazbadar@users.noreply.github.com>
2024-05-06 21:36:14 +0530

    css styling

--- app.py
+++ app.py
@@ -8,18 +8,13 @@ def index():
     message = get_flashed_messages(with_categories=True)
     return render_template('index.html', message=message)
 
-#test code for button
+#test code for start scan button
 @app.route('/scan', methods=['POST'])
-def scan():
+def scan(): #triggered when button is pressed
     if request.method == 'POST':
         return "Scanning network..."
     else:
         return "Invalid request method"
-
-@app.route('/downloadButton', methods=['POST'])
-def show_popup():
-    flash('This is a popup message!', 'downloading file...') 
-    return redirect(url_for('index'))  # Redirect back to the index page
 
 if __name__ == "__main__":
     app.run(debug=True)
